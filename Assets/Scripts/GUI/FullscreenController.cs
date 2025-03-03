14df308ec3c60bac177a58a6dda8de36aef36f6e
Harris-Bailey <161182629+Harris-Bailey@users.noreply.github.com>
2025-03-03 02:50:07 +0000

    Rectifying Fullscreen Controller Now opens in fullscreen depending on users preferences Changed the settings panel to inactive in the hierarchy

--- Assets/Scripts/GUI/FullscreenController.cs
+++ Assets/Scripts/GUI/FullscreenController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class FullscreenController : MonoBehaviour {
     
     private void Awake() {
+        Screen.fullScreen = SettingsSaveLoad.FullscreenActive;
         SettingsSaveLoad.OnAnySettingChanged += UpdateFullscreen;
     }
     
